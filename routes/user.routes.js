934a12f67ccbf1f19b1122219a745dd8737a4699
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-08 07:35:40 +0200

    Add new endpoints: profile, profile/settings, logout and add userName with photo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,11 +3,32 @@ const passport = require("passport");
 const router = express.Router();
 
 router.get("/logged", (req, res) => {
-  res.render("logged");
+  if (req.user) {
+    res.render("logged", {
+      userName: req.user.displayName,
+      photo: req.user.photos[0].value,
+    });
+  } else res.redirect("user/no-permission");
+});
+
+router.get("/profile", (req, res) => {
+  if (req.user) {
+    res.render("profile");
+  } else res.redirect("user/no-permission");
+});
+
+router.get("/profile/settings", (req, res) => {
+  if (req.user) {
+    res.render("profileSettings");
+  } else res.redirect("user/no-permission");
 });
 
 router.get("/no-permission", (req, res) => {
   res.render("noPermission");
+});
+
+router.get("/logout", (req, res) => {
+  res.render("logout");
 });
 
 module.exports = router;
